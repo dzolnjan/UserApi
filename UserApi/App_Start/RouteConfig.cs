49a5c3ec48f55901367cbecb3d32eb9edce4798e
Daniel Zolnjan <daniel.zolnjan@gmail.com>
2013-01-18 03:05:12 +0100

    init

---
+++ UserApi/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace UserApi
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Signup",
+                url: "api/v5/user/signup",
+                defaults: new { controller = "User", action = "Signup" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+
+        }
+    }
+}
---
+++ UserApi/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using UserApi.Models;
+using WebMatrix.WebData;
+
+namespace UserApi.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        [HttpPost]
+        public ActionResult Signup(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Attempt to register the user
+                try
+                {
+                    //WebSecurity.CreateUserAndAccount(model.Email, model.Password);
+                    var token = Guid.NewGuid();
+                    return Json(new { TokenId = token.ToString() });
+                }
+                catch (MembershipCreateUserException e)
+                {
+                    return Json(new { TokenId = "Already registered email." });
+                }
+            }
+
+            return Json(new { TokenId = "Email or password is missing." });
+
+        }
+
+    }
+}
